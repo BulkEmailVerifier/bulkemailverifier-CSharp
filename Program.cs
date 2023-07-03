076ff21b6ff13d52c77c191c4146a34c3bf44adb
proweb7 <45687254+proweb7@users.noreply.github.com>
2023-07-03 13:58:16 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,158 +1,42 @@
 using System;
 using System.IO;
-using System.Text;
-using System.Net;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-namespace ConsoleApplication1
+using System.Net.Http;
+
+namespace Bounceless
 {
     class Program
     {
-        /// <summary>
-        /// Together with the AcceptAllCertifications method right
-        /// below this causes to bypass errors caused by SLL-Errors.
-        /// </summary>
-        public static void IgnoreBadCertificates()
-        {
-            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(AcceptAllCertifications);
-        }
+        static readonly HttpClient client = new HttpClient();
 
-        /// <summary>
-        /// In Short: the Method solves the Problem of broken Certificates.
-        /// Sometime when requesting Data and the sending Webserverconnection
-        /// is based on a SSL Connection, an Error is caused by Servers whoes
-        /// Certificate(s) have Errors. Like when the Cert is out of date
-        /// and much more... So at this point when calling the method,
-        /// this behaviour is prevented
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="certification"></param>
-        /// <param name="chain"></param>
-        /// <param name="sslPolicyErrors"></param>
-        /// <returns>true</returns>
-        private static bool AcceptAllCertifications(object sender, X509Certificate certification, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-        {
-            return true;
-        }
-        /*
-         * ********** Function to verfiy one email ****************************************
-         */
-        public static void verifyByOne()
-        {
-            string key = "PUT YOUR KEY HERE";
-            string email = "emailtoverify@example.com";
-            string sURL = "https://apps.bounceless.io/api/verifyEmail?secret=" + key + "&email=" + email;
-            WebRequest wrGETURL;
-            wrGETURL = WebRequest.Create(sURL);
-
-            WebProxy myProxy = new WebProxy("myproxy", 80);
-            myProxy.BypassProxyOnLocal = true;
-
-            wrGETURL.Proxy = WebProxy.GetDefaultProxy();
-
-            Stream objStream;
-            objStream = wrGETURL.GetResponse().GetResponseStream();
-
-            StreamReader objReader = new StreamReader(objStream);
-
-            string sLine = "";
-            int i = 0;
-
-            while (sLine != null)
-            {
-                i++;
-                sLine = objReader.ReadLine();
-                if (sLine != null)
-                    Console.WriteLine("{0}:{1}", i, sLine);
-            }
-            Console.ReadLine();
-        }
-        /*
-         * ********** Function to check state after upload file****************************************
-         */
-        private static void CheckFileStatus(string id)
-        {
-            string key = "PUT YOUR KEY HERE";
-            string sURL = "https://apps.bounceless.io/api/getApiFileInfo?secret=" + key + "&id=" + id;
-            WebRequest wrGETURL;
-            wrGETURL = WebRequest.Create(sURL);
-
-            WebProxy myProxy = new WebProxy("myproxy", 80);
-            myProxy.BypassProxyOnLocal = true;
-
-            wrGETURL.Proxy = WebProxy.GetDefaultProxy();
-
-            Stream objStream;
-            objStream = wrGETURL.GetResponse().GetResponseStream();
-
-            StreamReader objReader = new StreamReader(objStream);
-
-            string sLine = "";
-            int i = 0;
-
-            while (sLine != null)
-            {
-                i++;
-                sLine = objReader.ReadLine();
-                if (sLine != null)
-                    Console.WriteLine("{0}:{1}", i, sLine);
-            }
-        }
-        /*
-         * ******************************************************************************
-         */
         static void Main(string[] args)
         {
-            //verifyByOne();
-            //string file_contents ="@/home/Downloads/emails.txt"; //path to your file
-            //string url = "http://ukr.net/api/verifApiFile?secret=" + key + "&filename=my_emails.txt";
+            VerifySingleEmail("PUT YOUR KEY HERE", "emailtoverify@example.com");
+            UploadFile("PUT YOUR KEY HERE", "path\\to\\your\\file.csv");
+            GetFileInfo("PUT YOUR KEY HERE", "FILE_ID");
+        }
 
-            //I use a method to ignore bad certs caused by misc errors
-            IgnoreBadCertificates();
+        static void VerifySingleEmail(string key, string email)
+        {
+            string url = $"https://apps.bounceless.io/api/singlemaildetails?secret={key}&email={email}";
+            var response = client.GetAsync(url).Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
 
-            string key = "PUT YOUR KEY HERE";
+        static void UploadFile(string key, string filePath)
+        {
+            string url = $"https://apps.bounceless.io/api/verifyApiFile?secret={key}&filename={Path.GetFileName(filePath)}";
+            using var content = new MultipartFormDataContent();
+            using var fileStream = File.OpenRead(filePath);
+            content.Add(new StreamContent(fileStream), "file", Path.GetFileName(filePath));
+            var response = client.PostAsync(url, content).Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
 
-            string file_contents = "c:\\php\\emails.txt";
-            string url = "https://apps.bounceless.io/api/verifyApiFile?secret=" + key + "&filename=my_emails.txt";
-
-            string boundary = "----------------------------" + DateTime.Now.Ticks.ToString("x");
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.ContentType = "multipart/form-data; boundary=" + boundary;
-            request.Method = "POST";
-            //request.ServicePoint.Expect100Continue = false;
-            request.KeepAlive = false;
-
-            string header = "--" + boundary + "\r\n" + "Content-Disposition: form-data; name=\"file_contents\"; filename=\""+file_contents+"\"\r\nContent-Type: application/octet-stream\r\n\r\n";
-
-            string trailer = "\r\n--" + boundary + "--\r\n";
-
-            //name="file_contents"; filename="c:\php\emails.txt"
-
-            byte[] bytesHeader = Encoding.ASCII.GetBytes(header);
-            byte[] bytesFileContent = File.ReadAllBytes(file_contents);
-            byte[] bytesTrailer = Encoding.ASCII.GetBytes(trailer);
-
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(bytesHeader, 0, bytesHeader.Length);
-            requestStream.Write(bytesFileContent, 0, bytesFileContent.Length);
-            requestStream.Write(bytesTrailer, 0, bytesTrailer.Length);
-            requestStream.Close();
-
-            try
-            {
-                using (StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream()))
-                {
-                    string id = reader.ReadToEnd();
-                    CheckFileStatus(id);
-
-                };
-            }
-            catch (Exception ex)
-            {
-                // something strange...
-            }
-
+        static void GetFileInfo(string key, string fileId)
+        {
+            string url = $"https://apps.bounceless.io/api/getApiFileInfo?secret={key}&id={fileId}";
+            var response = client.GetAsync(url).Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
         }
     }
 }
